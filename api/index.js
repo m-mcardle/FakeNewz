78bc3f064f5d2f3edfb27e9322a60fd01816fa12
Matt McArdle <matthew.j.mcardle@gmail.com>
2024-05-22 11:42:24 -0400

    Vercel seems to be working

--- api/index.js
+++ api/index.js
@@ -1,35 +1,49 @@
 // Import necessary packages
 const express = require('express');
 const ejs = require('ejs');
-const bodyParser = require('body-parser');
+const path = require('path');
 
 const firebase = require('./firebase');
 const { collection, getFirestore, doc, getDoc, addDoc } = require('firebase/firestore');
+const querystring = require('querystring');
 
 
 // Initialize Express app
 const app = express();
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
-
 // Initialize Firebase
 const db = getFirestore(firebase);
 
+// Manually parse URL encoded body because Vercel was throwing up doing it itself
+const parseURLEncodedBody = (req) => {
+  return new Promise((resolve, reject) => {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      try {
+        resolve(querystring.parse(body));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+};
+
 // Set view engine
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views')); // Set the views directory
 
 // Routes
 app.post('/article', async (req, res) => {
 
   try {
+    const body = await parseURLEncodedBody(req);
     const docRef = await addDoc(collection(db, 'Articles'), {
-      title: req.body.title,
-      description: req.body.description,
-      image: req.body.image,
+      title: body.title,
+      description: body.description,
+      image: body.image,
     });
 
     console.log('Document written with ID: ', docRef.id);
@@ -70,5 +84,11 @@ app.get('/images/:image', (req, res) => {
   res.sendFile(__dirname + '/images/' + req.params.image);
 });
 
-// Start server
-app.listen(3000, () => console.log('Server started on port 3000'));
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
